93c8b22bbfc7fcca2de8d1c84972525561790fd8
Satoe Imaishi <simaishi@redhat.com>
2020-05-06 13:28:16 -0400

    Support for customizing all repo urls and branches

--- lib/manageiq/rpm_build/setup_source_repos.rb
+++ lib/manageiq/rpm_build/setup_source_repos.rb
@@ -7,13 +7,11 @@ module ManageIQ
     class SetupSourceRepos
       include Helper
 
-      attr_reader :git_ref, :github_url, :repo_prefix
+      attr_reader :git_ref
 
       def initialize(ref)
         where_am_i
-        @git_ref     = ref || OPTIONS.git_ref
-        @github_url  = OPTIONS.github_url
-        @repo_prefix = OPTIONS.repo_prefix
+        @git_ref = ref || OPTIONS.repos.ref
       end
 
       def populate
@@ -31,22 +29,16 @@ module ManageIQ
 
       def setup_rpm_spec_repo
         where_am_i
-        FileUtils.mkdir_p RPM_SPEC_DIR
-        FileUtils.cp_r("/build_scripts/rpm_spec", BUILD_DIR)
-        Dir.chdir(RPM_SPEC_DIR) do
-          #git_clone("#{github_url}/#{OPTIONS.product_name}-gemset.git")
-          #git_clone("#{github_url}/#{OPTIONS.product_name}.git")
-          #git_clone("#{github_url}/#{OPTIONS.product_name}-appliance.git")
-        end
+        FileUtils.cp_r("/build_scripts/rpm_spec", RPM_SPEC_DIR)
       end
 
       def setup_source_repo
         where_am_i
         Dir.chdir(BUILD_DIR) do
-          git_clone("#{github_url}/#{repo_prefix}-appliance-build.git", "manageiq-appliance-build")
-          git_clone("#{github_url}/#{repo_prefix}-appliance.git", "manageiq-appliance")
-          git_clone("#{github_url}/#{repo_prefix}.git", "manageiq")
-          git_clone("#{github_url}/#{repo_prefix}-ui-service.git", "manageiq-ui-service")
+          git_clone(OPTIONS.repos.manageiq_appliance_build, "manageiq-appliance-build")
+          git_clone(OPTIONS.repos.manageiq_appliance, "manageiq-appliance")
+          git_clone(OPTIONS.repos.manageiq, "manageiq")
+          git_clone(OPTIONS.repos.manageiq_ui_service, "manageiq-ui-service")
         end
         # WORKAROUND
         FileUtils.cp(ROOT_DIR.join("evm_override"), BUILD_DIR.join("manageiq-appliance/LINK/etc/default/evm"))
@@ -63,9 +55,9 @@ module ManageIQ
 
       private
 
-      def git_clone(repo_url, destination = nil)
-        destination ||= File.basename(repo_url, ".git")
-        exit $?.exitstatus unless system("git clone --depth 1 -b #{git_ref} #{repo_url} #{destination}")
+      def git_clone(repo_options, destination)
+        repo_ref = repo_options.ref || git_ref
+        shell_cmd("git clone --depth 1 -b #{repo_ref} #{repo_options.url} #{destination}")
       end
     end
   end
