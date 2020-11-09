14e3af654133e46dd11e59d2bf205cc6bb143c72
Satoe Imaishi <simaishi@redhat.com>
2020-11-09 13:04:23 -0500

    Merge pull request #116 from Fryguy/allow_bundler_to_install
    Allow bundler to do the install with the correct build options
    
    (cherry picked from commit 0f8f85208c656989608a8493d5aac0439ce12242)

--- lib/manageiq/rpm_build/generate_gemset.rb
+++ lib/manageiq/rpm_build/generate_gemset.rb
@@ -65,8 +65,8 @@ module ManageIQ
           shell_cmd("gem install mime-types -v 2.6.1")
 
           if RUBY_PLATFORM.match?(/powerpc64le|s390x/)
-            shell_cmd("gem install sassc  -- --disable-march-tune-native")
-            shell_cmd("gem install unf_ext -- --with-cxxflags='-fsigned-char'")
+            shell_cmd("bundle config set build.sassc --disable-march-tune-native")
+            shell_cmd("bundle config set build.unf_ext --with-cxxflags='-fsigned-char'")
           end
 
           shell_cmd("bundle config set --local with qpid_proton systemd")
