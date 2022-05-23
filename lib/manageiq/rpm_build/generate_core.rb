ffc624eeeb00c192cfa4bc281b5d25f8df698236
Minal-Mahor <minal.mahor1997@gmail.com>
2022-05-23 07:37:08 -0700

    skip git clean on z

--- lib/manageiq/rpm_build/generate_core.rb
+++ lib/manageiq/rpm_build/generate_core.rb
@@ -61,7 +61,7 @@ module ManageIQ
           shell_cmd("yarn install")
           shell_cmd("yarn run available-languages")
           shell_cmd("yarn run build")
-          shell_cmd("git clean -xdf")  # cleanup temp files
+          shell_cmd("git clean -xdf") if !RUBY_PLATFORM.include?("s390x") 
         end
       end
 
