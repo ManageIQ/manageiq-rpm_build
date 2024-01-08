942991ce5d1268fd82e27b0ed76771ab24ed172c
Joe Rafaniello <jrafanie@gmail.com>
2024-01-08 13:52:44 -0500

    Use built-in vcs for excluding version control files

--- lib/manageiq/rpm_build/generate_tar_files.rb
+++ lib/manageiq/rpm_build/generate_tar_files.rb
@@ -22,14 +22,14 @@ module ManageIQ
         plugin_index.write(plugin_index.read.gsub(BUILD_DIR.join("manageiq").to_s, '/var/www/miq/vmdb'))
 
         name = "gemset"
-        shell_cmd("tar -C #{BUILD_DIR} -zcf #{tar_full_path(name)} -X #{exclude_file(name)} #{tar_basename(name)}")
+        shell_cmd("tar -C #{BUILD_DIR} -zcf #{tar_full_path(name)} --exclude-vcs -X #{exclude_file(name)} #{tar_basename(name)}")
       end
 
       def create_appliance_tarball
         where_am_i
 
         name = "appliance"
-        shell_cmd("tar -C #{BUILD_DIR.join("manageiq-appliance")} #{transform(name)} --exclude='.git' -hzcf #{tar_full_path(name)} .")
+        shell_cmd("tar -C #{BUILD_DIR.join("manageiq-appliance")} #{transform(name)} --exclude-vcs -hzcf #{tar_full_path(name)} .")
       end
 
       def create_core_tarball
@@ -38,21 +38,21 @@ module ManageIQ
         name = "core"
 
         # Everything from */tmp/* should be excluded, except for tmp/cache/sti_loader.yml
-        shell_cmd("tar -C #{BUILD_DIR.join("manageiq")} #{transform(name)} --exclude-tag='cache/sti_loader.yml' -X #{exclude_file("manageiq")} -hczf #{tar_full_path(name)} .")
+        shell_cmd("tar -C #{BUILD_DIR.join("manageiq")} #{transform(name)} --exclude-vcs --exclude-tag='cache/sti_loader.yml' -X #{exclude_file("manageiq")} -hczf #{tar_full_path(name)} .")
       end
 
       def create_ansible_venv_tarball
         where_am_i
 
         name = "ansible-venv"
-        shell_cmd("tar -C #{BUILD_DIR.join("manageiq-ansible-venv")} #{transform(name)} -X #{exclude_file(name)} -hzcf #{tar_full_path(name)} .")
+        shell_cmd("tar -C #{BUILD_DIR.join("manageiq-ansible-venv")} #{transform(name)} --exclude-vcs -X #{exclude_file(name)} -hzcf #{tar_full_path(name)} .")
       end
 
       def create_manifest_tarball
         where_am_i
 
         name = "manifest"
-        shell_cmd("tar -C #{BUILD_DIR.join(name)} #{transform(name)} --exclude='.git' -hzcf #{tar_full_path(name)} .")
+        shell_cmd("tar -C #{BUILD_DIR.join(name)} #{transform(name)} --exclude-vcs -hzcf #{tar_full_path(name)} .")
       end
 
       private
